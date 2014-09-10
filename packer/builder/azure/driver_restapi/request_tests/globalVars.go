d744df8ce3a04a3e5012abdd6a75f5a125499b8a
v-vlshch <shcherbakov.mailbox@gmail.com>
2014-09-09 17:51:51 -0700

    file headers updated

--- packer/builder/azure/driver_restapi/request_tests/globalVars.go
+++ packer/builder/azure/driver_restapi/request_tests/globalVars.go
@@ -1,3 +1,8 @@
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+// See License.txt in the project root for license information.
+
 package request_tests
 
 var g_tmpServiceName string = "pkrsrvawn4zu9fv8"
