bca61520e9624a0dfc84cd11c378985a2efe66aa
Paul Meyer <paulmey@microsoft.com>
2014-10-06 22:45:12 -0700

    generate random passwords for windows VMs

--- packer/builder/azure/utils/BuildAzureVmName.go
+++ packer/builder/azure/utils/BuildAzureVmName.go
@@ -7,23 +7,53 @@ package utils
 import (
 	"math/rand"
 	"os"
-	"sync"
 	"time"
 )
 
 const availableSymbols = "0123456789abcdefghijklmnopqrstuvwxyz"
+
+var pwSymbols = []string{
+	"abcdefghijklmnopqrstuvwxyz",
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	"0123456789",
+}
+
 const allowedVmNameLength = 15
 
-var randmu sync.Mutex
-
 func BuildAzureVmNameRandomSuffix(prefix string) (suffix string) {
-	randmu.Lock()
-	rand.Seed(time.Now().UnixNano() + int64(os.Getpid()))
-	availableSymbolsLen := len(availableSymbols)
 	genLen := allowedVmNameLength - len(prefix)
-	for i := 0; i < genLen; i++ {
-		suffix += string(availableSymbols[rand.Intn(availableSymbolsLen)])
-	}
-	randmu.Unlock()
+	suffix += RandomString(availableSymbols, genLen)
 	return
+}
+
+var rnd = rand.New(rand.NewSource(time.Now().UnixNano() + int64(os.Getpid())))
+
+func RandomString(chooseFrom string, length int) (randomString string) {
+	cflen := len(chooseFrom)
+	for i := 0; i < length; i++ {
+		randomString += string(chooseFrom[rnd.Intn(cflen)])
+	}
+	return
+}
+
+func RandomPassword() (password string) {
+	pwlen := 15
+	batchsize := pwlen / len(pwSymbols)
+	pw := make([]byte, 0, pwlen)
+	// choose character set
+	for c := 0; len(pw) < pwlen; c++ {
+		s := RandomString(pwSymbols[c%len(pwSymbols)], rnd.Intn(batchsize-1)+1)
+		pw = append(pw, []byte(s)...)
+	}
+	// truncate
+	pw = pw[:pwlen]
+
+	// permute
+	for c := 0; c < pwlen-1; c++ {
+		i := rnd.Intn(pwlen-c) + c
+		x := pw[c]
+		pw[c] = pw[i]
+		pw[i] = x
+	}
+	return string(pw)
 }
