9328adfc886d1f844b59cb137e4624c5f33c4e23
Paul Meyer <paul.meyer@microsoft.com>
2015-06-29 15:36:50 -0700

    Add test for vnet/subnet

--- packer/builder/azure/config_test.go
+++ packer/builder/azure/config_test.go
@@ -8,20 +8,10 @@ import (
 	"testing"
 )
 
-func TestConfig_newConfig(t *testing.T) {
-	f, err := ioutil.TempFile("", "")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err = f.Close(); err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(f.Name())
-
-	log.SetOutput(testLogger{t}) // hide log if test is succes
-	cfg, _, err := newConfig(map[string]string{
+func getDefaultTestConfig(publishSettingsFileName string) map[string]string {
+	return map[string]string{
 		"subscription_name":         "My subscription",
-		"publish_settings_path":     f.Name(),
+		"publish_settings_path":     publishSettingsFileName,
 		"storage_account":           "mysa",
 		"storage_account_container": "vhdz",
 		"os_type":                   "Linux",
@@ -29,7 +19,15 @@ func TestConfig_newConfig(t *testing.T) {
 		"location":                  "Central US",
 		"instance_size":             "Large",
 		"user_image_label":          "boo",
-	})
+	}
+}
+
+func TestConfig_newConfig(t *testing.T) {
+	f := getTempFile(t)
+	defer os.Remove(f)
+	log.SetOutput(testLogger{t}) // hide log if test is succes
+
+	cfg, _, err := newConfig(getDefaultTestConfig(f))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -38,6 +36,41 @@ func TestConfig_newConfig(t *testing.T) {
 	if !regexp.MustCompile(expectedPattern).MatchString(cfg.userImageName) {
 		t.Errorf("expected %q to match %q", cfg.userImageName, expectedPattern)
 	}
+}
+
+func TestConfig_VNet(t *testing.T) {
+	f := getTempFile(t)
+	defer os.Remove(f)
+	log.SetOutput(testLogger{t}) // hide log if test is succes
+
+	tcs := []struct {
+		cfgmod func(map[string]string)
+		err    bool
+	}{
+		{func(cfg map[string]string) { cfg["vnet"] = "vnetName" }, true},
+		{func(cfg map[string]string) { cfg["subnet"] = "subnetName" }, true},
+		{func(cfg map[string]string) { cfg["vnet"] = "vnetName"; cfg["subnet"] = "subnetName" }, false},
+	}
+
+	for _, tc := range tcs {
+		cfgmap := getDefaultTestConfig(f)
+		tc.cfgmod(cfgmap)
+		_, _, err := newConfig(cfgmap)
+		if (err != nil) != tc.err {
+			t.Fatalf("unexpected error value: %v", err)
+		}
+	}
+}
+
+func getTempFile(t *testing.T) string {
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = f.Close(); err != nil {
+		t.Fatal(err)
+	}
+	return f.Name()
 }
 
 type testLogger struct{ *testing.T }
