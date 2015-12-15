647a06cd39a4956159e181c034b1dd41b0d0bb6e
Christopher Boumenot <christopher.boumenot@microsoft.com>
2015-12-14 16:02:34 -0800

    Class to hold template parameters.

---
+++ packer/builder/azure/arm/template_parameters.go
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+// See License.txt in the project root for license information.
+
+package arm
+
+// The intent of these types to facilitate interchange with Azure in the
+// appropriate JSON format. A sample format is below.  Each parameter listed
+// below corresponds to a parameter defined in the template.
+//
+// {
+//   "storageAccountName": {
+//     "value" : "my_storage_account_name"
+//   },
+//   "adminUserName" : {
+//     "value": "admin"
+//   }
+// }
+
+type TemplateParameter struct {
+	Value string `json:"value"`
+}
+
+type TemplateParameters struct {
+	AdminUsername *TemplateParameter `json:"adminUsername,omitempty"`
+	DnsNameForPublicIP *TemplateParameter `json:"dnsNameForPublicIP,omitempty"`
+	ImageOffer *TemplateParameter `json:"imageOffer,omitempty"`
+	ImagePublisher *TemplateParameter `json:"imagePublisher,omitempty"`
+	ImageSku *TemplateParameter `json:"imageSku,omitempty"`
+	Location *TemplateParameter `json:"location,omitempty"`
+	OSDiskName *TemplateParameter `json:"osDiskName,omitempty"`
+	SshAuthorizedKey *TemplateParameter `json:"sshAuthorizedKey,omitempty"`
+	StorageAccountName *TemplateParameter `json:"storageAccountName,omitempty"`
+	VMSize *TemplateParameter `json:"vmSize,omitempty"`
+	VMName *TemplateParameter `json:"vmName,omitempty"`
+}
