f2f56f3c5239ef0e5ad23c80a5d91016e1aa0fbd
Christopher Boumenot <christopher.boumenot@microsoft.com>
2016-01-06 15:36:18 -0800

    Implement delete OS disk step.

---
+++ packer/builder/azure/arm/step_delete_os_disk.go
@@ -0,0 +1,69 @@
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+// See License.txt in the project root for license information.
+
+package arm
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/Azure/packer-azure/packer/builder/azure/common/constants"
+
+	"github.com/mitchellh/multistep"
+	"github.com/mitchellh/packer/packer"
+)
+
+type StepDeleteOSDisk struct {
+	client *AzureClient
+	delete func(string, string) error
+	say    func(message string)
+	error  func(e error)
+}
+
+func NewStepDeleteOSDisk(client *AzureClient, ui packer.Ui) *StepDeleteOSDisk {
+	var step = &StepDeleteOSDisk{
+		client: client,
+		say:    func(message string) { ui.Say(message) },
+		error:  func(e error) { ui.Error(e.Error()) },
+	}
+
+	step.delete = step.deleteBlob
+	return step
+}
+
+func (s *StepDeleteOSDisk) deleteBlob(storageContainerName string, blobName string) error {
+	return s.client.BlobStorageClient.DeleteBlob(storageContainerName, blobName)
+}
+
+func (s *StepDeleteOSDisk) Run(state multistep.StateBag) multistep.StepAction {
+	s.say("Deleting the temporary OS disk ...")
+
+	var osDisk = state.Get(constants.ArmOSDiskVhd).(string)
+	s.say(fmt.Sprintf(" -> OS Disk             : '%s'", osDisk))
+
+	u, err := url.Parse(osDisk)
+	if err != nil {
+		s.say("Failed to parse the OS Disk's VHD URI!")
+		return multistep.ActionHalt
+	}
+
+	xs := strings.Split(u.Path, "/")
+
+	var storageAccountName = strings.Join(xs[1:len(xs)-1], "/")
+	var blobName = xs[len(xs)-1]
+
+	err = s.delete(storageAccountName, blobName)
+	if err != nil {
+		state.Put(constants.Error, err)
+		s.error(err)
+
+		return multistep.ActionHalt
+	}
+	return multistep.ActionContinue
+}
+
+func (*StepDeleteOSDisk) Cleanup(multistep.StateBag) {
+}
