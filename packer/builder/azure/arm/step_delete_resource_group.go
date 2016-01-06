8d31b10055aebe086d715f6e85d08922e11ac987
Christopher Boumenot <christopher.boumenot@microsoft.com>
2016-01-06 15:40:04 -0800

    Implement delete resource group step.

---
+++ packer/builder/azure/arm/step_delete_resource_group.go
@@ -0,0 +1,63 @@
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+// See License.txt in the project root for license information.
+
+package arm
+
+import (
+	"fmt"
+
+	"github.com/Azure/packer-azure/packer/builder/azure/common/constants"
+	"github.com/mitchellh/multistep"
+	"github.com/mitchellh/packer/packer"
+)
+
+type StepDeleteResourceGroup struct {
+	client *AzureClient
+	delete func(resourceGroupName string) error
+	say    func(message string)
+	error  func(e error)
+}
+
+func NewStepDeleteResourceGroup(client *AzureClient, ui packer.Ui) *StepDeleteResourceGroup {
+	var step = &StepDeleteResourceGroup{
+		client: client,
+		say:    func(message string) { ui.Say(message) },
+		error:  func(e error) { ui.Error(e.Error()) },
+	}
+
+	step.delete = step.deleteResourceGroup
+	return step
+}
+
+func (s *StepDeleteResourceGroup) deleteResourceGroup(resourceGroupName string) error {
+	res, err := s.client.GroupsClient.Delete(resourceGroupName)
+	if err != nil {
+		return err
+	}
+
+	s.client.GroupsClient.PollAsNeeded(res.Response)
+	return nil
+}
+
+func (s *StepDeleteResourceGroup) Run(state multistep.StateBag) multistep.StepAction {
+	s.say("Deleting resource group ...")
+
+	var resourceGroupName = state.Get(constants.ArmResourceGroupName).(string)
+
+	s.say(fmt.Sprintf(" -> ResourceGroupName : '%s'", resourceGroupName))
+
+	err := s.delete(resourceGroupName)
+	if err != nil {
+		state.Put(constants.Error, err)
+		s.error(err)
+
+		return multistep.ActionHalt
+	}
+
+	return multistep.ActionContinue
+}
+
+func (*StepDeleteResourceGroup) Cleanup(multistep.StateBag) {
+}
