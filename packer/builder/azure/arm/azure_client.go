caaf0c2105b980288d683c53fa9ea5709b3e55ef
Christopher Boumenot <christopher.boumenot@microsoft.com>
2016-01-06 14:09:46 -0800

    One Azure client to rule them all.

---
+++ packer/builder/azure/arm/azure_client.go
@@ -0,0 +1,55 @@
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+// See License.txt in the project root for license information.
+
+package arm
+
+import (
+	"github.com/Azure/azure-sdk-for-go/Godeps/_workspace/src/github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/azure-sdk-for-go/arm/compute"
+	"github.com/Azure/azure-sdk-for-go/arm/network"
+	"github.com/Azure/azure-sdk-for-go/arm/resources"
+	armStorage "github.com/Azure/azure-sdk-for-go/arm/storage"
+	"github.com/Azure/azure-sdk-for-go/storage"
+)
+
+type AzureClient struct {
+	compute.VirtualMachinesClient
+	network.PublicIPAddressesClient
+	resources.GroupsClient
+	resources.DeploymentsClient
+	storage.BlobStorageClient
+}
+
+func NewAzureClient(subscriptionID string, resourceGroupName string, storageAccountName string, servicePrincipalToken *azure.ServicePrincipalToken) (*AzureClient, error) {
+	var azureClient = &AzureClient{}
+
+	azureClient.DeploymentsClient = resources.NewDeploymentsClient(subscriptionID)
+	azureClient.DeploymentsClient.Authorizer = servicePrincipalToken
+
+	azureClient.GroupsClient = resources.NewGroupsClient(subscriptionID)
+	azureClient.GroupsClient.Authorizer = servicePrincipalToken
+
+	azureClient.PublicIPAddressesClient = network.NewPublicIPAddressesClient(subscriptionID)
+	azureClient.PublicIPAddressesClient.Authorizer = servicePrincipalToken
+
+	azureClient.VirtualMachinesClient = compute.NewVirtualMachinesClient(subscriptionID)
+	azureClient.VirtualMachinesClient.Authorizer = servicePrincipalToken
+
+	accountsClient := armStorage.NewAccountsClient(subscriptionID)
+	accountsClient.Authorizer = servicePrincipalToken
+
+	accountKeys, err := accountsClient.ListKeys(resourceGroupName, storageAccountName)
+	if err != nil {
+		return nil, err
+	}
+
+	storageClient, err := storage.NewBasicClient(storageAccountName, *accountKeys.Key1)
+	if err != nil {
+		return nil, err
+	}
+
+	azureClient.BlobStorageClient = storageClient.GetBlobService()
+	return azureClient, nil
+}
